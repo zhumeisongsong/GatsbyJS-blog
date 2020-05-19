2ff48d0cbc62da5238af690a96b6412ed32a574a
zhumeisongsong <zhumei.song@monstar-lab-cn.com>
2020-05-19 15:21:00 +0900

    chnage file to typescript

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, PageProps } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = (props: PageProps) => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
